d0a80a4202fce4c4f45e32ccfb13cd07a14ebdbf
Jhonyans <143343847+Jhonyans@users.noreply.github.com>
2023-09-07 07:38:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function togglemode() {
+
+    const html = document.documentElement
+    if(html.classList.contains("light")) { 
+        html.classList.remove("light")
+  } else { html.classList.add("light")
+  }
+}
+
+const img = document.querySelector("#.vscode/style.css")
+if (html.classList.contains("light")) {
+    img.setAttribute("src", "./projeto/projeto/Eu !.jpg")
+} else {
+    img.setAttribute("src", "./projeto/projeto/Eu !.jpg")  
+}
